ae13c9b8518bc18596d5929c35fa7974216f4c93
eyradel <60956082+eyradel@users.noreply.github.com>
2023-02-28 15:48:18 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,1 @@
+auth_key = "f868d0690bb847e4b876705fdc83b0e9"
---
+++ untitled0.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 22 21:18:11 2023
+
+@author: eyram
+"""
+number  = int(input("enter a number"))
+
+if number%2==0:
+    print("the number is even")
+else:
+    print("number is odd")
+
